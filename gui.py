783956a2669d830ecb7973f3f79366e379257362
Vin Kam <vinkamat10@gmail.com>
2025-07-20 01:11:54 +0530

    implement the Edit button with some bug need to review

--- gui.py
+++ gui.py
@@ -4,8 +4,12 @@ import FreeSimpleGUI as sg
 label = sg.Text("Type in a to-do")
 input_box = sg.InputText(tooltip="Enter Todo", key="todo")
 add_button = sg.Button("Add")
+list_box = sg.Listbox(values=functions.get_todos(), key="current_todos",
+                      enable_events=True, size=(45, 10))
+edit_button = sg.Button("Edit")
 
-window = sg.Window('My To-Do App', layout=[[label], [input_box, add_button]],
+window = sg.Window('My To-Do App',
+                   layout=[[label], [input_box, add_button], [list_box, edit_button]],
                    font=('Helvetica', 20))
 
 while True:
@@ -18,6 +22,18 @@ while True:
             new_todo = values['todo'] + "\n"
             todos.append(new_todo)
             functions.write_todos(todos)
+            window['current_todos'].update(values=todos)
+        case "Edit":
+            new_todo = values["todo"] + "\n"
+            todo_to_edit = values["current_todos"][0]
+            todos = functions.get_todos()
+            index = todos.index(todo_to_edit)
+            todos[index] = new_todo
+            functions.write_todos(todos)
+            window['current_todos'].update(values=todos)
+        case "current_todos":
+            window["todo"].update(value=values["current_todos"][0])
+
         case sg.WIN_CLOSED:
             break
 
