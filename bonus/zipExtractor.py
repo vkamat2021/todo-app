c47f1bd025e544c720cef2420e38f38043fa46f6
Vin Kam <vinkamat10@gmail.com>
2025-07-22 00:03:33 +0530

    initial commit of ZipExtractor app

---
+++ bonus/zipExtractor.py
@@ -0,0 +1,8 @@
+import zipfile
+
+def extract_archive(archivepath, dest_dir):
+    with zipfile.ZipFile(archivepath, 'r') as archive:
+        archive.extractall(dest_dir)
+
+if __name__ == "__main__":
+    extract_archive("dest/compressed.zip","files2")
