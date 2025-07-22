1380074a061d4f00b514d7d9f3334321df28c80e
Vin Kam <vinkamat10@gmail.com>
2025-07-22 23:45:08 +0530

    Added feature to handle adsense of input file for packaging the program

--- gui.py
+++ gui.py
@@ -1,6 +1,11 @@
 import functions
 import FreeSimpleGUI as sg
 import time
+import os
+
+if not os.path.exists("todo.txt"):
+    with open("todos.txt", "w") as file:
+        pass
 
 sg.theme("Black")
 label_clock = sg.Text('', key='clock')
