99a238e81f9a9ac6ef393b5fafc3aad3c76028f4
Vin Kam <vinkamat10@gmail.com>
2025-07-22 21:27:06 +0530

    implement Exit button and handle window close

--- bonus/ConvertUsMetricGui.py
+++ bonus/ConvertUsMetricGui.py
@@ -1,6 +1,7 @@
 from bonus.convertUsMetric_func_convert import convert
 import FreeSimpleGUI as sg
 
+sg.theme("Black")
 label_feet = sg.Text("Enter feet: ")
 input_feet = sg.Input(key="feet")
 
@@ -8,18 +9,25 @@ label_inches = sg.Text("Enter inches: ")
 input_inches = sg.Input(key="inches")
 
 convert_button = sg.Button("Convert")
+exit_button = sg.Button("Exit")
 output_label = sg.Text("", key='output')
 
 window = sg.Window("Convertor", layout=[[label_feet, input_feet],
                                         [label_inches, input_inches],
-                                        [convert_button, output_label]])
+                                        [convert_button, exit_button, output_label]])
 
 while True:
     event, values = window.read()
+    match event:
+        case "Exit":
+            break
+        case sg.WIN_CLOSED:
+            break
+
     output = convert(float(values['feet']), float(values['inches']))
     result = f"{output}m"
     window['output'].update(value=result)
-    # print(output)
-    # print(event, values)
+    print(output)
+    print(event, values)
 
 window.close()
