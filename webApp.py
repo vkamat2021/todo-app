c000b5bdb5ea793fd5ff7bd5d9bc7551a6129920
Vin Kam <vinkamat10@gmail.com>
2025-07-24 10:19:22 +0530

    implement complete todo item

--- webApp.py
+++ webApp.py
@@ -14,8 +14,16 @@ st.title("My Todo App")
 st.subheader("this is my todo app")
 st.write("This app is to increase your productivity")
 
-for todo in todos:
-    st.checkbox(todo)
+for index, todo in enumerate(todos):
+    checkbox = st.checkbox(todo, key=todo)
+    if checkbox:
+        todos.pop(index)
+        functions.write_todos(todos)
+        del st.session_state[todo]
+        st.rerun()
+
 
 st.text_input(label="Enter a todo: ", placeholder="Add new todo..",
               on_change=add_todo, key='new_todo')
+
+st.session_state
