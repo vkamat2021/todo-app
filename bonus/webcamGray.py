ae5f82d5d458f2905b3668771530fed2838142a7
Vin Kam <vinkamat10@gmail.com>
2025-07-25 20:45:55 +0530

    Implemented image upload for the  Webcamera Grayscale app

--- bonus/webcamGray.py
+++ bonus/webcamGray.py
@@ -1,6 +1,21 @@
 import streamlit as st
 from PIL import Image
 
+st.subheader("Color to Grayscale Converter")
+
+# Create a file uploader component allowing the user to upload a file
+with st.expander("Upload image"):
+    # Upload image
+    uploaded_image = st.file_uploader("Upload Image")
+
+if  uploaded_image:
+    # Create a pillow image instance
+    img = Image.open(uploaded_image)
+    # Convert the pillow image to grayscale
+    gray_img = img.convert("L")
+    # Render the graysclae image on the web page
+    st.image(gray_img)
+
 with st.expander("Start camera"):
     # Start the camera
     camera_image = st.camera_input("Camera")
